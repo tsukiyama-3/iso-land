c667825e4a049cf0e24ca295f354e7a23946cdbe
tsukiyama-3 <tsukiyama.mail@gmail.com>
2025-09-08 00:24:32 +0900

    setting: eslint

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
+  modules: ['@nuxt/eslint'],
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint']
+  compatibilityDate: '2025-07-15',
+  eslint: {
+    config: {
+      stylistic: true,
+    },
+  },
 })
