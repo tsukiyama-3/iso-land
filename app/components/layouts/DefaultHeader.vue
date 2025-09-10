9678ba9251d2199b64d3937658776d84a4a6a457
tsukiyama-3 <tsukiyama.mail@gmail.com>
2025-09-11 00:02:04 +0900

    fix: style

--- app/components/layouts/DefaultHeader.vue
+++ app/components/layouts/DefaultHeader.vue
@@ -11,5 +11,49 @@
         height=""
       >
     </template>
+    <template #right>
+      <div class="hidden md:block">
+        <UButton
+          icon="i-simple-icons-x"
+          color="neutral"
+          variant="ghost"
+          size="xl"
+          to="https://x.com/tsuyakima3"
+          target="_blank"
+          aria-label="X"
+        />
+        <UButton
+          icon="i-simple-icons-github"
+          color="neutral"
+          variant="ghost"
+          size="xl"
+          to="https://github.com/tsukiyama-3/iso-land"
+          target="_blank"
+          aria-label="GitHub"
+        />
+      </div>
+    </template>
+    <template #body>
+      <div class="flex items-center justify-center">
+        <UButton
+          icon="i-simple-icons-x"
+          color="neutral"
+          variant="ghost"
+          size="xl"
+          to="https://x.com/tsuyakima3"
+          target="_blank"
+          aria-label="X"
+        />
+        <UButton
+          icon="i-simple-icons-github"
+          color="neutral"
+          variant="ghost"
+          size="xl"
+          to="https://github.com/tsukiyama-3/iso-land"
+          target="_blank"
+          aria-label="GitHub"
+        />
+      </div>
+    </template>
   </UHeader>
 </template>
--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -8,30 +8,5 @@ import DefaultHeader from '~/components/layouts/DefaultHeader.vue'
     <main class="grow py-4">
       <slot />
     </main>
-    <UFooter :ui="{ container: 'py-4' }">
-      <template #left>
-        <p class="text-muted text-sm">
-          Copyright © {{ new Date().getFullYear() }}
-        </p>
-      </template>
-      <template #right>
-        <UButton
-          icon="i-simple-icons-x"
-          color="neutral"
-          variant="ghost"
-          to="https://x.com/tsuyakima3"
-          target="_blank"
-          aria-label="X"
-        />
-        <UButton
-          icon="i-simple-icons-github"
-          color="neutral"
-          variant="ghost"
-          to="https://github.com/tsukiyama-3/iso-land"
-          target="_blank"
-          aria-label="GitHub"
-        />
-      </template>
-    </UFooter>
   </div>
 </template>
