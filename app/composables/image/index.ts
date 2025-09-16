fc95a3df1ce6623534b9d33a66b227c2f952f199
tsukiyama-3 <tsukiyama.mail@gmail.com>
2025-09-17 01:17:34 +0900

    fix: message

--- app/composables/image/index.ts
+++ app/composables/image/index.ts
@@ -60,7 +60,7 @@ export const useImage = () => {
     {
       role: 'assistant',
       type: 'text',
-      content: '1日5回まで画像を生成できます。画像生成には10秒ほどかかります。',
+      content: '画像生成には10秒ほどかかります。',
     },
   ])
 
